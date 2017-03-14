9f1746392fea2faade31d467608c767a25281535
Shubhashish Verma <svashish305@gmail.com>
2017-03-14 17:16:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req,res) {
    counter = counter + 1;
-   res.send(couner.toString()); 
+   res.send(counter.toString()); 
 });
 
 app.get('/:articleName', function (req, res) {
