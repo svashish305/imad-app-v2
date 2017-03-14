88e0a42fff7a61f39b294fd17871606b5ced7c53
Shubhashish Verma <svashish305@gmail.com>
2017-03-14 14:28:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML = 'New Value';
 //Move the image
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 var img = document.getElementById('madi');
