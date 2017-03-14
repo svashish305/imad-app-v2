e6b627429a892a63f55688781f52aae4299c63a0
Shubhashish Verma <svashish305@gmail.com>
2017-03-14 17:15:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req,res) {
+   counter = counter + 1;
+   res.send(couner.toString()); 
+});
+
 app.get('/:articleName', function (req, res) {
   //articleName == article-one
   //articles[articleName] = {} content object for article one
