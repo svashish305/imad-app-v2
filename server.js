d16dcb5306bac67b830a21b3c7d0cd0ee7fa81fb
Shubhashish Verma <svashish305@gmail.com>
2017-03-15 17:49:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config)
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
    // make a select request
    // return the response with the results
