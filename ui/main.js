73fe15c7d5a706a24d845f01efc7ef4835951440
Shubhashish Verma <svashish305@gmail.com>
2017-03-17 18:06:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ submit.onclick = function () {
     console.log(username);
     console.log(password);
     request.open('POST', 'http://svashish305.imad.hasura-app.io/login', true);
-    request.setRequestHeader('Content-Type: application/json');
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
     
 };
