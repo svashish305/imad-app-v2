15c2e1f12d2a65fdcea3e9cc0880d7bf96863873
Shubhashish Verma <svashish305@gmail.com>
2017-03-17 17:33:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,30 @@ app.post('/create-user', function (req, res) {
            res.send('User successfully created: ' + username);
        }       
    });
-   
+});
+
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   pool.query('SELECT * FROM "user" WHERE username=$1', [username], function (err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           if(result.rows.length === 0) {
+               res.status(403).send('usernmae/password is invalid');
+           } else {
+               // Match the password
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hash(password, salt); // creating a hash based on the password submitted and the original salt
+               if (hashedPassword === dbString) {
+               res.send('credentials correct!');
+               } else {
+                   res.status(403).send('usernmae/password is invalid');
+               }
+           }    
+           }       
+   });    
 });
 
 var pool = new Pool(config);
